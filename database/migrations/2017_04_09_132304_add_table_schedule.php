c620d5c332cb72f8e791060bf2edfc2214256f82
Blaz Cigale <blaz.cigale@gmail.com>
2017-04-09 15:34:59 +0200

    Added db migrations

---
+++ database/migrations/2017_04_09_132304_add_table_schedule.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTableSchedule extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('activity_id')->unsigned();
+            $table->dateTime('activity_start');
+            $table->foreign('activity_id')->references('id')->on('activities')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

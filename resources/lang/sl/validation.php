456d04ad3c985e5b7cb1f68c81f7349f0ac46610
Blaz Cigale <blaz.cigale@gmail.com>
2017-04-09 18:23:16 +0000

    Edited messages validation file

--- resources/lang/sl/validation.php
+++ resources/lang/sl/validation.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'csv.file'      => 'Datoteka se ni naložila. Poskusite ponovno',
-    'csv.mimes'     => 'Datoteka ni pravilnega formata',
-    'csv.required'  => 'Izbrati morate datoteko',
+    'csv.file'          => 'Datoteka se ni naložila. Poskusite ponovno',
+    'csv.mimes'         => 'Datoteka ni pravilnega formata',
+    'csv.mimetypes'     => 'Datoteka ni pravilnega formata',
+    'csv.required'      => 'Izbrati morate datoteko',
 ];

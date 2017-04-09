aed4ef233309166abc6622c0e83dadff41324275
Blaz Cigale <blaz.cigale@gmail.com>
2017-04-09 17:15:01 +0000

    Added custom Request

---
+++ app/Http/Requests/ImportRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ImportRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'csv' => 'required|file|mimes:csv',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'csv.file'      => __('validation.csv.file'),
+            'csv.mimes'     => __('validation.csv.mimes'),
+            'csv.required'  => __('validation.csv.required'),
+        ];
+    }
+}
